9aabd535cdb8fbad9f224b0b8131e409cd6dbd21
nathleegrant-lang <nathleegrant@gmail.com>
2026-04-27 09:41:34 -0500

    Create site-config.ts

---
+++ lib/site-config.ts
@@ -0,0 +1,10 @@
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL || "https://grade-5-pep.vercel.app"
+
+export const PRODUCT_CODE =
+  process.env.NEXT_PUBLIC_PRODUCT_CODE || "grade5"
+
+export const PRODUCT_LABEL = "Grade 5 PEP"
+
+export const WHATSAPP_NUMBER = "18765055212"
+export const WHATSAPP_DISPLAY = "876-505-5212"
