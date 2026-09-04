96be0609827194009ddc11e4f3b40801a678aea8
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 15:15:15 -0500

    Apply Grade 5 visual system to Social Studies hub

---
+++ app/social-studies/layout.tsx
@@ -0,0 +1,5 @@
+import "../subject-hubs.css"
+
+export default function SocialStudiesLayout({ children }: { children: React.ReactNode }) {
+  return <div className="grade5-social-studies-hub">{children}</div>
+}
