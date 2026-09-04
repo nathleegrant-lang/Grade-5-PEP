801c96f054f5b82ca215f4b3699add90d8f25bb0
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 15:15:21 -0500

    Apply Grade 5 visual system to Performance Tasks hub

---
+++ app/performance-tasks/layout.tsx
@@ -0,0 +1,5 @@
+import "../subject-hubs.css"
+
+export default function PerformanceTasksLayout({ children }: { children: React.ReactNode }) {
+  return <div className="grade5-performance-hub">{children}</div>
+}
