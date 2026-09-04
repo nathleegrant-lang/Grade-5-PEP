af74572596c33b354aa2564545ced98d7ba1bfc3
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 16:29:00 -0500

    Load subject section hierarchy refinement

--- app/social-studies/layout.tsx
+++ app/social-studies/layout.tsx
@@ -1,4 +1,5 @@
 import "../subject-hubs.css"
+import "../subject-hubs-refinement.css"
 
 export default function SocialStudiesLayout({ children }: { children: React.ReactNode }) {
   return <div className="grade5-social-studies-hub">{children}</div>
