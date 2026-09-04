83c39d67b00127cc545729473b23a262aa9ba59a
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 16:28:17 -0500

    Load Language Arts section hierarchy refinement

--- app/language-arts/layout.tsx
+++ app/language-arts/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react"
 import "./prototype.css"
+import "./section-refinement.css"
 
 export default function LanguageArtsPrototypeLayout({ children }: { children: ReactNode }) {
   return <div className="grade5-language-arts-prototype">{children}</div>
