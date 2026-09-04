a562f01d75d866f40534dc9e16fd758fb5e846ab
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 16:28:54 -0500

    Load subject section hierarchy refinement

--- app/science/layout.tsx
+++ app/science/layout.tsx
@@ -1,4 +1,5 @@
 import "../subject-hubs.css"
+import "../subject-hubs-refinement.css"
 
 export default function ScienceLayout({ children }: { children: React.ReactNode }) {
   return <div className="grade5-science-hub">{children}</div>
