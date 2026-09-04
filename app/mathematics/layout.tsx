ba6adad4d32c93da304e36e79e60529ce89fe302
nathleegrant-lang <nathleegrant@gmail.com>
2026-09-04 15:15:00 -0500

    Apply Grade 5 visual system to Mathematics hub

---
+++ app/mathematics/layout.tsx
@@ -0,0 +1,5 @@
+import "../subject-hubs.css"
+
+export default function MathematicsLayout({ children }: { children: React.ReactNode }) {
+  return <div className="grade5-mathematics-hub">{children}</div>
+}
